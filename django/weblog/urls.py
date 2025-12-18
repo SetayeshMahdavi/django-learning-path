2804403e952886cb098b3129fa187c1b059002cd
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2025-12-18 13:33:14 +0330

    add a post detail template

--- django/weblog/urls.py
+++ django/weblog/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import index ,home,post_list
+from posts.views import index ,home,post_list,post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),   
     path('index/',index),
     path('home/',home),
-    path('posts/',post_list,name='post_title')
+    path('posts/',post_list,name='post_title'),
+    path('posts/<int:post_id>/',post_detail,name='post_detail')
 ]
