9f678a8d0ca2bbb87116e8cada1b0e99ed71a334
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2026-01-03 20:59:25 +0330

    work on api part

--- django/posts/models.py
+++ django/posts/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
+
+class PostLiveManager(models.Manager):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_enable=True)
+        return queryset
+
+
+
+
 class Post(models.Model):
     title=models.CharField(max_length=50)
     text=models.TextField(blank=True)
     is_enable=models.BooleanField(default=False)
     publish_date=models.DateField(null=True,blank=True)
     created_time=models.DateTimeField(auto_now_add=True)
-    updated_time=models.DateTimeField(auto_now=True) 
+    updated_time=models.DateTimeField(auto_now=True)
+
+
+    objects = models.Manager()
+    live=PostLiveManager()
+
  
     def __str__(self):
         #return self.title
