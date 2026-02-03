dbbe9e07f6bcf8fe639d52515efcc7f519685554
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2026-02-03 16:31:13 +0330

    some work on API& DRF

--- posts/views.py
+++ posts/views.py
@@ -13,3 +13,21 @@ class PostListView(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class PostDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
