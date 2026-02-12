a29f607935c71990988a7c457bf0013e4a9aaf68
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2026-02-12 15:19:44 +0330

    base Post view on GenericAPIView

--- posts/serializer.py
+++ posts/serializer.py
@@ -5,4 +5,5 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id','title', 'text', 'is_enable', 'publish_date']
+        fields='__all__'
+        #fields = ['id','title', 'text', 'is_enable', 'publish_date']
--- posts/views.py
+++ posts/views.py
@@ -1,33 +1,82 @@
+
+from django.http import Http404
+
 from rest_framework.views import APIView  #genericview&listapiview
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
 from rest_framework import status
 
 from .models import Post
-from rest_framework.decorators import api_view
-
 from .serializer import PostSerializer
 
+# class PostListView(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+#
+# class PostDetailView(APIView):
+#     def grt_object(self,pk):
+#         try:
+#             post = Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             raise Http404
+#         return post
+#
+#     def get(self, request, pk):
+#         post = self.grt_object(pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         post = self.grt_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         post = self.grt_object(pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class PostListView(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostListView(generics.GenericAPIView,
+                   mixins.ListModelMixin,
+                   mixins.CreateModelMixin, ):
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-class PostDetailView(APIView):
-    def get(self, request, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+class PostDetailView(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
