4bdca0ca5ded9b240b2c7fbc69d86631ae63812c
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2026-02-14 19:39:55 +0330

    refactor: optimize list and detail views using generics

--- posts/views.py
+++ posts/views.py
@@ -10,73 +10,13 @@ from rest_framework import status
 from .models import Post
 from .serializer import PostSerializer
 
-# class PostListView(APIView):
-#     def get(self, request):
-#         posts = Post.objects.all()
-#         serializer = PostSerializer(posts, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request):
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-#
-# class PostDetailView(APIView):
-#     def grt_object(self,pk):
-#         try:
-#             post = Post.objects.get(pk=pk)
-#         except Post.DoesNotExist:
-#             raise Http404
-#         return post
-#
-#     def get(self, request, pk):
-#         post = self.grt_object(pk)
-#         serializer = PostSerializer(post)
-#         return Response(serializer.data)
-#
-#     def put(self, request, pk):
-#         post = self.grt_object(pk)
-#         serializer = PostSerializer(post, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk):
-#         post = self.grt_object(pk)
-#         post.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class PostListView(generics.GenericAPIView,
-                   mixins.ListModelMixin,
-                   mixins.CreateModelMixin, ):
-
+class postListview(generics.ListCreateAPIViewAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class PostDetailView(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class postDetailview(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
